9709384f613d385541b3c5b5d6c49c51597db593
Christine Su <christine.hui.su@gmail.com>
2021-04-17 12:22:16 -0400

    first commit

---
+++ db.js
@@ -0,0 +1,83 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const Sequelize = require("sequelize");
+const { STRING } = Sequelize;
+const config = {
+  logging: false,
+};
+
+if (process.env.LOGGING) {
+  delete config.logging;
+}
+const conn = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost/acme_db",
+  config
+);
+
+const User = conn.define("user", {
+  username: STRING,
+  password: STRING,
+});
+
+User.addHook("beforeSave", async function (user) {
+  user.password = await bcrypt.hash(user.password, 20);
+});
+
+User.byToken = async (token) => {
+  try {
+    const { id } = await jwt.verify(token, process.env.JWT);
+    const user = await User.findByPk(id);
+    if (user) {
+      return user;
+    }
+    const error = Error("bad credentials");
+    error.status = 401;
+    throw error;
+  } catch (ex) {
+    const error = Error("bad credentials");
+    error.status = 401;
+    throw error;
+  }
+};
+
+User.authenticate = async ({ username, password }) => {
+  const user = await User.findOne({
+    where: {
+      username,
+      password,
+    },
+  });
+  if (user) {
+    // return user.id;
+    return jwt.sign({ id: user.id }, process.env.JWT);
+  }
+  const error = Error("bad credentials");
+  error.status = 401;
+  throw error;
+};
+
+const syncAndSeed = async () => {
+  await conn.sync({ force: true });
+  const credentials = [
+    { username: "lucy", password: "lucy_pw" },
+    { username: "moe", password: "moe_pw" },
+    { username: "larry", password: "larry_pw" },
+  ];
+  const [lucy, moe, larry] = await Promise.all(
+    credentials.map((credential) => User.create(credential))
+  );
+  return {
+    users: {
+      lucy,
+      moe,
+      larry,
+    },
+  };
+};
+
+module.exports = {
+  syncAndSeed,
+  models: {
+    User,
+  },
+};
